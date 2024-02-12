b27adb78c177a16ccf7c3b99a6939e25ef558bd1
zeinab Yourdkhani <tarjomic1401@gmail.com>
2024-02-12 10:25:54 +0330

    add message

--- js/script.js
+++ js/script.js
@@ -1,32 +1,34 @@
 let $ = document;
-let addNote = $.querySelector('#addNote');
-let allNote = $.querySelector('#allNote');
-let color = $.querySelectorAll('.color');
-let inputNote = $.querySelector('#inputNote');
-
+let addNote = $.querySelector("#addNote");
+let allNote = $.querySelector("#allNote");
+let color = $.querySelectorAll(".color");
+let inputNote = $.querySelector("#inputNote");
+let message = $.querySelector("#message");
 
 color.forEach(function (newColor) {
-    newColor.addEventListener('click', function (event) {
-        let userColor = event.target.style.backgroundColor;
-        $.body.style.backgroundColor = userColor
-    });
+  newColor.addEventListener("click", function (event) {
+    let userColor = event.target.style.backgroundColor;
+    $.body.style.backgroundColor = userColor;
+  });
 });
-addNote.addEventListener('click', function () {
-    if (inputNote.value === '') {
-    } else {
-        let newNote = $.createElement('li');
-        newNote.classList.add('newNoteClass');
-        let txtNote = $.createElement('span');
-        let deleteNote = $.createElement('i');
-        deleteNote.className = 'fa fa-trash-o text-danger pl-3';
-        deleteNote.style.fontSize = '1.4rem'
-        deleteNote.addEventListener('click', function (event) {
-            event.target.parentElement.remove()
-        });
-        newNote.innerHTML = ''
-        allNote.append(newNote);
-        newNote.append(txtNote, deleteNote);
-        txtNote.innerHTML = inputNote.value;
-        inputNote.value = "";
-    }
+addNote.addEventListener("click", function () {
+  if (inputNote.value === "") {
+    message.style.display = "block";
+  } else {
+    message.style.display = "none";
+    let newNote = $.createElement("li");
+    newNote.classList.add("newNoteClass");
+    let txtNote = $.createElement("span");
+    let deleteNote = $.createElement("i");
+    deleteNote.className = "fa fa-trash-o text-danger pl-3 cursor-pointer";
+    deleteNote.style.fontSize = "1.4rem";
+    deleteNote.addEventListener("click", function (event) {
+      event.target.parentElement.remove();
+    });
+    newNote.innerHTML = "";
+    allNote.append(newNote);
+    newNote.append(txtNote, deleteNote);
+    txtNote.innerHTML = inputNote.value;
+    inputNote.value = "";
+  }
 });
